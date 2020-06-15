08c5b63c244829a60b27bf1661c3e02e76aba4ea
Ronald Runia <rrunia@gmail.com>
2020-06-15 11:23:20 +0200

    show private products to logged in users

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
+import netlifyIdentity from 'netlify-identity-widget';
 
 import Layout from '../components/layout';
 
-const Products = ({ data: { allContentfulProduct } }) => (
-  <Layout>
-    <div>
-      <h2>Garb Products</h2>
-      {allContentfulProduct.edges.map(({ node: product }) => (
-        <div key={product.id}>
-          <h2>
-            <Link to={`/products/${product.slug}`}>
-              {product.name} | &euro; {product.price}
-            </Link>
-          </h2>
-          <Img style={{ maxWidth: 240 }} fluid={product.image.fluid} />
-        </div>
-      ))}
-    </div>
-  </Layout>
-);
+const Products = ({ data: { allContentfulProduct } }) => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () =>
+    netlifyIdentity.currentUser() !== null
+      ? allContentfulProduct.edges
+      : allContentfulProduct.edges.filter(
+          ({ node: product }) => !product.private
+        );
+
+  useEffect(() => {
+    setProducts(getProducts());
+  }, []);
+
+  return (
+    <Layout>
+      <div>
+        <h2>Garb Products</h2>
+        {products.map(({ node: product }) => (
+          <div key={product.id}>
+            <h2>
+              <Link to={`/products/${product.slug}`}>
+                {product.name} | &euro; {product.price}
+              </Link>
+            </h2>
+            <Img style={{ maxWidth: 240 }} fluid={product.image.fluid} />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default Products;
 
@@ -33,6 +49,7 @@ export const query = graphql`
           id
           name
           price
+          private
           image {
             fluid(maxWidth: 800) {
               ...GatsbyContentfulFluid
